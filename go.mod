c44648a786cb747abbca59e707f6938779319bd4
vv198x <vv198x@gmail.com>
2022-11-27 19:11:44 +0500

    add postgree

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module benchSQLs
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
--- main_test.go
+++ main_test.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	//_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
@@ -16,10 +17,19 @@ CREATE TABLE `tickets` (
 `state` varchar(255) NOT NULL DEFAULT 'open',
 PRIMARY KEY (`id`)
 )
+
+CREATE TABLE "tickets" (
+
+	id int check (id > 0) NOT NULL ,
+	  subdomain_id int NOT NULL,
+	  subject varchar(255) NOT NULL DEFAULT '',
+	  state varchar(255) NOT NULL DEFAULT 'open',
+	  PRIMARY KEY (id)
+	  )
 */
 var multip = 10000
 
-func BenchmarkSimplest(b *testing.B) {
+func bBenchmarkSimplest(b *testing.B) {
 	b.ResetTimer()
 	db, err := sql.Open("mysql", os.Getenv("MARIADB")+"/test")
 	if err != nil {
@@ -27,6 +37,31 @@ func BenchmarkSimplest(b *testing.B) {
 	}
 	for n := 0; n < b.N*multip; n++ {
 		rows, err := db.Query("select id, subject, state from tickets where subdomain_id = ? and (state = ? or state = ?) limit 1", 1, "open", "spam")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var id int
+		var subject, state string
+		for rows.Next() {
+			err := rows.Scan(&id, &subject, &state)
+			//log.Println(id, subject, state)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		rows.Close()
+	}
+	db.Close()
+}
+
+func BenchmarkSimplestPostgre(b *testing.B) {
+	b.ResetTimer()
+	db, err := sql.Open("postgres", "host=192.168.122.111 user=sql password=test dbname=test sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for n := 0; n < b.N*multip; n++ {
+		rows, err := db.Query("select id, subject, state from TICKETS limit 1")
 		if err != nil {
 			log.Fatalln(err)
 		}
