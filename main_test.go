43ead59f43e0cf6a1d215dcdc894fd28c3dcdbde
vv198x <vv198x@gmail.com>
2022-11-27 20:52:17 +0500

    end test

--- main_test.go
+++ main_test.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"database/sql"
-	//_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	//_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
@@ -27,24 +28,55 @@ CREATE TABLE "tickets" (
 	  PRIMARY KEY (id)
 	  )
 */
-var multip = 10000
 
-func bBenchmarkSimplest(b *testing.B) {
+func benchmarkMariaDB(b *testing.B, lim int) {
 	b.ResetTimer()
 	db, err := sql.Open("mysql", os.Getenv("MARIADB")+"/test")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for n := 0; n < b.N*multip; n++ {
-		rows, err := db.Query("select id, subject, state from tickets where subdomain_id = ? and (state = ? or state = ?) limit 1", 1, "open", "spam")
+	for n := 1; n <= lim; n++ {
+		st := fmt.Sprintf("INSERT into tickets(id, subdomain_id,subject,state) VALUES (%v,%v,'1','1')", n, n)
+		_, err := db.Exec(st)
+		rows, err := db.Query("select id, subject, state from tickets")
 		if err != nil {
-			log.Fatalln(err)
+			fmt.Println(err)
 		}
 		var id int
 		var subject, state string
 		for rows.Next() {
 			err := rows.Scan(&id, &subject, &state)
-			//log.Println(id, subject, state)
+			st = fmt.Sprintf("DELETE FROM tickets WHERE id = %v ", id)
+			_, err = db.Exec(st)
+			if err != nil {
+				fmt.Println(err)
+
+			}
+		}
+		rows.Close()
+	}
+	db.Close()
+}
+
+/*
+func benchmarkSimplestPostgres(b *testing.B, lim int) {
+	b.ResetTimer()
+	db, err := sql.Open("postgres", "host=192.168.122.111 user=sql password=test dbname=test sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for n := 1; n <= lim; n++ {
+		st := fmt.Sprintf("INSERT into tickets(id, subdomain_id,subject,state) VALUES (%v,%v,'1','1')", n, n)
+		_, err := db.Exec(st)
+		rows, err := db.Query("select id, subject, state from tickets")
+		if err != nil {
+			fmt.Println(err)
+		}
+		var id int
+		var subject, state string
+		for rows.Next() {
+			err := rows.Scan(&id, &subject, &state)
+			db.Exec("DELETE from tickets WHERE id=$1", id)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -53,28 +85,23 @@ func bBenchmarkSimplest(b *testing.B) {
 	}
 	db.Close()
 }
+func BenchmarkPostgres50(b *testing.B) {
+	benchmarkSimplestPostgres(b, 50)
+}
+func BenchmarkPostgres100(b *testing.B) {
+	benchmarkSimplestPostgres(b, 100)
+}
+func BenchmarkPostgres200(b *testing.B) {
+	benchmarkSimplestPostgres(b, 200)
+}
+*/
 
-func BenchmarkSimplestPostgre(b *testing.B) {
-	b.ResetTimer()
-	db, err := sql.Open("postgres", "host=192.168.122.111 user=sql password=test dbname=test sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for n := 0; n < b.N*multip; n++ {
-		rows, err := db.Query("select id, subject, state from TICKETS limit 1")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		var id int
-		var subject, state string
-		for rows.Next() {
-			err := rows.Scan(&id, &subject, &state)
-			//log.Println(id, subject, state)
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}
-		rows.Close()
-	}
-	db.Close()
+func BenchmarkPostgres50(b *testing.B) {
+	benchmarkMariaDB(b, 50)
+}
+func BenchmarkPostgres100(b *testing.B) {
+	benchmarkMariaDB(b, 100)
+}
+func BenchmarkPostgres200(b *testing.B) {
+	benchmarkMariaDB(b, 200)
 }
