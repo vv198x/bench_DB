c13fe25dc0f7e46b8c88b0e8372b03cbd7f8091a
vv198x <vv198x@gmail.com>
2023-01-20 16:18:03 +0500

    end test

--- README.md
+++ README.md
@@ -1,30 +1,67 @@
-RESULT:
-mariadb
+# Benchmark Test for MongoDB and JSONB (pg)
+This benchmark test compares the performance of MongoDB and JSONB when inserting, updating, reading and deleting documents.
+
+### Test Setup
+The test was performed on a machine with the following specifications:
+
+* CPU: intel core i3 9100f
+* RAM: 16GB DDR4
+* Storage: 256GB SSD
+* OS: Fedora 37
+The MongoDB and JSONB databases were run on the same machine, using the latest version of their respective software.
+
+### Test Results
+The test results are shown in the table below:
+
+| Test Name       | Number of Documents | Time (ns/op)         | Memory (B/op) | Allocs/op |
+|-----------------|---------------------|----------------------|----------------|-----------|
+| BenchmarkMongo1 | 1                   | 0.03156 ns/op        | 0 B/op         | 0         |
+| BenchmarkMongo100 | 100                | 0.4614 ns/op         | 0 B/op         | 0         |
+| BenchmarkMongo200 | 200                | 0.9224 ns/op         | 0 B/op         | 0         |
+| BenchmarkMongo500 | 500                | 2076665187 ns/op    | 15726880 B/op  | 251912    |
+| BenchmarkJSONB1 | 1                   | 0.01976 ns/op        | 0 B/op         | 0         |
+| BenchmarkJSONB100 | 100                | 204185849 ns/op      | 136963 B/op    | 2044      |
+| BenchmarkJSONB200 | 200                | 2617058157 ns/op     | 1292336 B/op   | 20323     |
+| BenchmarkJSONB500 | 500                | 6457528499 ns/op     | 3113936 B/op   | 50623     |
+
+* MongoDB tests took about 0.03 seconds for 1 document, 0.46 seconds for 100 documents, and 0.92 seconds for 200 documents. 
+* JSONB tests took about 0.019 seconds for 1 document, 0.204 seconds for 100 documents, and 2.61 seconds for 200 documents.
+
+1. This is just one way to measure the performance of these technologies, and the results may vary depending on the specific use case and hardware being used. Other factors such as the complexity of the queries, the amount of data being stored and retrieved, and the number of concurrent requests being made can also affect performance.
+2. Maybe JSONB is more performant when working with big data, but MongoDB is more performant when dealing with small data. It depends on your use case and the amount of data that you need to manage.
+
+
+
+
+
+
+## OLD TEST(MariaDB vs Postgres) RESULT:
+### mariadb
 hard 136 mb
 mem 335 mb
 
-postgresql
+### postgresql
 hard 80
 mem 262
 
-posgres first: 20s
+### posgres first: 20s
 BenchmarkPostgres50-4   1000000000  0.3320      ns/op  0       B/op  0        allocs/op
 BenchmarkPostgres100-4  6128        172249      ns/op  23      B/op  0        allocs/op
 BenchmarkPostgres200-4  1           1545513570  ns/op  256448  B/op  6906     allocs/op
 
-postgres second: 8s
+### postgres second: 8s
 BenchmarkPostgres50-4   1000000000  0.3794      ns/op  0       B/op  0        allocs/op
 BenchmarkPostgres100-4  21          51429318    ns/op  6835    B/op  171      allocs/op
 BenchmarkPostgres200-4  1           1406782021  ns/op  256752  B/op  6909     allocs/op
 
 mem after 276
 
-mariadb first: 18s
+### mariadb first: 18s
 BenchmarkPostgres50-4   1000000000  0.3135      ns/op  0       B/op  0        allocs/op
 BenchmarkPostgres100-4  4622        220736      ns/op  24      B/op  0        allocs/op
 BenchmarkPostgres200-4  1           1264586084  ns/op  213152  B/op  6875     allocs/op
 
-mariadb second: 15s
+### mariadb second: 15s
 BenchmarkPostgres50-4   1000000000  0.3150      ns/op  0       B/op  0        allocs/op
 BenchmarkPostgres100-4  271         3899507     ns/op  422     B/op  12       allocs/op
 BenchmarkPostgres200-4  1           1576092967  ns/op  212048  B/op  6866     allocs/op
--- main_test.go
+++ main_test.go
@@ -1,14 +1,168 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	//_ "github.com/lib/pq"
+	"context"
+	"encoding/json"
+	"github.com/go-pg/pg"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
-	"os"
 	"testing"
+	"time"
 )
+
+func ConnectMongoDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(`mongodb://root:rootpassword@192.168.122.161:27017/`))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func benchmarkMongoDB(b *testing.B, lim int) {
+	client := ConnectMongoDB()
+	defer client.Disconnect(context.TODO())
+
+	collection := client.Database("test").Collection("benchmark")
+	var result bson.M
+
+	for i := 0; i < lim; i++ {
+		collection.InsertOne(context.TODO(), bson.M{
+			"name":  "test",
+			"value": "benchmark",
+			"attachments": []bson.M{
+				{
+					"name": "attachment1.jpg",
+					"type": "image/jpeg",
+					"data": "base64 encoded image data",
+				},
+				{
+					"name": "attachment2.pdf",
+					"type": "application/pdf",
+					"data": "base64 encoded pdf data",
+				},
+			},
+			"array": []bson.M{
+				{
+					"name":  "item1",
+					"value": "value1",
+				},
+				{
+					"name":  "item2",
+					"value": "value2",
+				},
+			},
+		})
+
+		collection.UpdateOne(context.TODO(), bson.M{"name": "test"}, bson.M{"$set": bson.M{"value": "updated"}})
+
+		collection.UpdateOne(context.TODO(),
+			bson.M{"name": "test"},
+			bson.M{
+				"$push": bson.M{
+					"array": bson.M{"name": "upd", "value": "updated_value"},
+				},
+			})
+
+		collection.FindOne(context.TODO(), bson.M{"name": "test"}).Decode(&result)
+
+		collection.DeleteOne(context.TODO(), bson.M{"name": "test"})
+	}
+
+}
+
+func BenchmarkMongo1(b *testing.B) {
+	benchmarkMongoDB(b, 1)
+}
+func BenchmarkMongo100(b *testing.B) {
+	benchmarkMongoDB(b, 100)
+}
+func BenchmarkMongo200(b *testing.B) {
+	benchmarkMongoDB(b, 200)
+}
+func BenchmarkMongo500(b *testing.B) {
+	benchmarkMongoDB(b, 500)
+}
+
+func BenchmarkHold(b *testing.B) {
+	time.Sleep(time.Second * 2)
+}
+
+type Benchmark struct {
+	ID          int
+	Name_       string
+	Value_      string
+	Attachments json.RawMessage
+	Array_      json.RawMessage
+}
+
+func init() {
+
+	connectPG().Exec(`CREATE TABLE IF NOT EXISTS benchmarks(
+    id SERIAL PRIMARY KEY,
+    name_ VARCHAR(255) NOT NULL,
+    value_ VARCHAR(255) NOT NULL,
+    attachments JSONB NOT NULL,
+    array_ JSONB NOT NULL
+);`)
+}
+func connectPG() *pg.DB {
+	return pg.Connect(&pg.Options{
+		Addr:     "192.168.122.161:5432",
+		User:     "postgres",
+		Password: "password",
+		Database: "database",
+	})
+}
+
+func benchmarkJSONB(b *testing.B, lim int) {
+	db := connectPG()
+	defer db.Close()
+	var result Benchmark
+
+	for i := 0; i < lim; i++ {
+
+		db.Model(&Benchmark{
+			Name_:       "test",
+			Value_:      "benchmark",
+			Attachments: json.RawMessage(`[{"name":"attachment1.jpg","type":"image/jpeg","data":"base64 encoded image data"},{"name":"attachment2.pdf","type":"application/pdf","data":"base64 encoded pdf data"}]`),
+			Array_:      json.RawMessage(`[{"name":"item1","value":"value1"},{"name":"item2","value":"value2"}]`),
+		}).Insert()
+
+		db.Model(&Benchmark{}).Set("value = ?", "updated").Where("name = ?", "test").Update()
+
+		db.Exec(`UPDATE benchmarks SET array_ = jsonb_set(array_, '{1,name}', '"upd"');`)
+		//db.Model(&Benchmark{}).Set(`array_ = jsonb_set(array_, '{1,name}', 'upd')`).Update()
+
+		db.Model(&result).Where("name = ?", "test").Select()
+
+		db.Model(&result).Where("name = ?", "test").Delete()
+
+	}
+
+}
+
+func BenchmarkJSONB1(b *testing.B) {
+	benchmarkJSONB(b, 1)
+}
+func BenchmarkJSONB100(b *testing.B) {
+	benchmarkJSONB(b, 100)
+}
+func BenchmarkJSONB200(b *testing.B) {
+	benchmarkJSONB(b, 200)
+}
+func BenchmarkJSONB500(b *testing.B) {
+	benchmarkJSONB(b, 500)
+}
 
 /*
 CREATE TABLE `tickets` (
@@ -27,9 +181,9 @@ CREATE TABLE "tickets" (
 	  state varchar(255) NOT NULL DEFAULT 'open',
 	  PRIMARY KEY (id)
 	  )
-*/
 
-func benchmarkMariaDB(b *testing.B, lim int) {
+
+func benchmarkMongoDB(b *testing.B, lim int) {
 	b.ResetTimer()
 	db, err := sql.Open("mysql", os.Getenv("MARIADB")+"/test")
 	if err != nil {
@@ -58,7 +212,6 @@ func benchmarkMariaDB(b *testing.B, lim int) {
 	db.Close()
 }
 
-/*
 func benchmarkSimplestPostgres(b *testing.B, lim int) {
 	b.ResetTimer()
 	db, err := sql.Open("postgres", "host=192.168.122.111 user=sql password=test dbname=test sslmode=disable")
@@ -95,13 +248,3 @@ func BenchmarkPostgres200(b *testing.B) {
 	benchmarkSimplestPostgres(b, 200)
 }
 */
-
-func BenchmarkPostgres50(b *testing.B) {
-	benchmarkMariaDB(b, 50)
-}
-func BenchmarkPostgres100(b *testing.B) {
-	benchmarkMariaDB(b, 100)
-}
-func BenchmarkPostgres200(b *testing.B) {
-	benchmarkMariaDB(b, 200)
-}
